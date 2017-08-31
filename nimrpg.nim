704dc2704ff9a8a4a6875b7a0ccc7d13c158c493
Jacob Moen <jacmoe.dk@gmail.com>
2017-08-31 19:17:09 +0200

    Proper Nim-kosher license headers...

--- nimrpg.nim
+++ nimrpg.nim
@@ -1,16 +1,16 @@
-#
-#   This file is part of the
-#   _   _ _           _____  _____   _____ 
-#  | \ | (_)         |  __ \|  __ \ / ____|
-#  |  \| |_ _ __ ___ | |__) | |__) | |  __ 
-#  | . ` | | '_ ` _ \|  _  /|  ___/| | |_ |
-#  | |\  | | | | | | | | \ \| |    | |__| |
-#  |_| \_|_|_| |_| |_|_|  \_\_|     \_____|
-# 
-#   project : https://github.com/jacmoe/nimrpg
-#
-#   Copyright 2017 Jacob Moen
-#
+#[
+   This file is part of the
+   _   _ _           _____  _____   _____ 
+  | \ | (_)         |  __ \|  __ \ / ____|
+  |  \| |_ _ __ ___ | |__) | |__) | |  __ 
+  | . ` | | '_ ` _ \|  _  /|  ___/| | |_ |
+  | |\  | | | | | | | | \ \| |    | |__| |
+  |_| \_|_|_| |_| |_|_|  \_\_|     \_____|
+ 
+   project : https://github.com/jacmoe/nimrpg
+
+   Copyright 2017 Jacob Moen
+]#
 import view, d20
 
 init("NimRPG", "Testing...\n")
