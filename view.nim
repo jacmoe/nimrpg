3d86bd30f5bda9442fb73676a1e8e1a42ff02929
Jacob Moen <jacmoe.dk@gmail.com>
2017-08-31 02:35:47 +0200

    Rooms

--- view.nim
+++ view.nim
@@ -20,6 +20,9 @@ var
   player_y = 0
 
 type
+  Rect = ref object of RootObj
+    x1, x2, y1, y2 : int
+
   Tile = ref object of RootObj
     blocked : bool
     block_sight : bool
@@ -35,17 +38,54 @@ var
   color_dark_wall : TColor = color_RGB(0, 0, 100)
   color_dark_ground : TColor = color_RGB(50, 50, 150)
 
-method move(what : Character, dx : int, dy : int) =
-  if not map[what.x + dx][what.y + dy].blocked:
-    what.x += dx
-    what.y += dy
+proc newRect(x : int, y : int, w : int, h : int) : Rect =
+  result = new Rect
+  result.x1 = x
+  result.y1 = y
+  result.x2 = x + w
+  result.y2 = y + h
 
-method draw(what : Character) =
-  console_set_default_foreground(main_console, what.color)
-  console_put_char(main_console, what.x, what.y, what.symbol, BKGND_NONE)
+proc create_room(room : Rect) =
+  # go through the tiles in the rectangle and make them passable
+  for i in room.x1 + 1..room.x2:
+    for j in room.y1 + 1..room.y2:
+      map[i][j].blocked = false
+      map[i][j].block_sight = false
 
-method clear(what : Character) =
-  console_put_char(main_console, what.x, what.y, ' ', BKGND_NONE)
+proc create_h_tunnel(x1 : int, x2 : int, y : int) =
+  for x in min(x1, x2)..(max(x1, x2) + 1):
+    map[x][y].blocked = false
+    map[x][y].block_sight = false
+
+proc create_v_tunnel(y1 : int, y2 : int, x : int) =
+  for y in min(y1, y2)..(max(y1, y2) + 1):
+    map[x][y].blocked = false
+    map[x][y].block_sight = false
+
+proc make_map =
+  # fill map with "blocked" tiles
+  for i in 0..<MAP_WIDTH:
+    for j in 0..<MAP_HEIGHT:
+      map[i][j] = Tile(blocked : true, block_sight: true)
+  
+  # create two rooms
+  var room1  = newRect(20, 15, 10, 15)
+  var room2 = newRect(50, 15, 10, 15)
+  create_room(room1)
+  create_room(room2)
+  create_h_tunnel(25, 55, 23)
+
+method move(self : Character, dx : int, dy : int) =
+  if not map[self.x + dx][self.y + dy].blocked:
+    self.x += dx
+    self.y += dy
+
+method draw(self : Character) =
+  console_set_default_foreground(main_console, self.color)
+  console_put_char(main_console, self.x, self.y, self.symbol, BKGND_NONE)
+
+method clear(self : Character) =
+  console_put_char(main_console, self.x, self.y, ' ', BKGND_NONE)
   
 
 proc init*(title : string, message: string) : void =
@@ -55,16 +95,11 @@ proc init*(title : string, message: string) : void =
 
   player =  Character(x : 0, y : 0, color : RED, symbol : '@')
 
-  for i in 0..<MAP_WIDTH:
-    for j in 0..<MAP_HEIGHT:
-      map[i][j] = Tile(blocked : false, block_sight: false)
+  make_map()
 
-  # place two pillars to test the map
-  map[30][22].blocked = true
-  map[30][22].block_sight = true
-  map[50][22].blocked = true
-  map[50][22].block_sight = true
-  
+  player.x = 25
+  player.y = 23
+
   console_clear(main_console)
   #discard console_print_rect_ex(main_console, SCREEN_WIDTH_2, 3, SCREEN_WIDTH, 0, BKGND_NONE, CENTER, message)
   
