747a9fbc11afc680912c25177bb707eccb8a6e8b
Jacob Moen <jacmoe.dk@gmail.com>
2017-08-31 17:54:31 +0200

    Clean up : making the code more attractive

--- view.nim
+++ view.nim
@@ -18,23 +18,37 @@ const
   SCREEN_WIDTH : int = 80
   SCREEN_HEIGHT : int = 50
   # map size
-  MAP_WIDTH = 80
-  MAP_HEIGHT = 45
+  MAP_WIDTH : int = 80
+  MAP_HEIGHT : int = 45
   # dungeon generation
-  ROOM_MAX_SIZE = 10
-  ROOM_MIN_SIZE = 6
-  MAX_ROOMS = 30
+  ROOM_MAX_SIZE : int = 10
+  ROOM_MIN_SIZE : int = 6
+  MAX_ROOMS : int = 30
+  # FOV
+  FOV_ALGO : int = 0
+  FOV_LIGHT_WALLS : bool = true
+  TORCH_RADIUS : int = 10
   # 20 frames per second limit
   LIMIT_FPS : int = 20
+  # colors
+  COLOR_LIGHT_WALL : TColor = color_RGB(130, 110, 50)
+  COLOR_DARK_WALL : TColor = color_RGB(0, 0, 100)
+  COLOR_LIGHT_GROUND : TColor = color_RGB(200, 180, 50)
+  COLOR_DARK_GROUND : TColor = color_RGB(50, 50, 150)
+  
 
 type
+  # Rectangle on the map, used to represent a room
   Rect = ref object of RootObj
     x1, x2, y1, y2 : int
 
+  # A tile of the map and its properties
   Tile = ref object of RootObj
     blocked : bool
     block_sight : bool
 
+  # Generic object represented by a character on the screen
+  # A Character can be: player, monster, item, stairs, ...
   Character = ref object of RootObj
     x, y : int
     color : TColor
@@ -42,13 +56,15 @@ type
 
 var
   main_console: PConsole
-  renderer = RENDERER_SDL
   key: TKey
   mouse: TMouse
   player : Character
   map : array[0..MAP_WIDTH, array[0..MAP_HEIGHT, Tile]]
-  color_dark_wall : TColor = color_RGB(0, 0, 100)
-  color_dark_ground : TColor = color_RGB(50, 50, 150)
+  fov_recompute : bool
+
+#########################################################################
+# Rect
+#########################################################################
 
 proc newRect(x : int, y : int, w : int, h : int) : Rect =
   result = new Rect
@@ -58,6 +74,7 @@ proc newRect(x : int, y : int, w : int, h : int) : Rect =
   result.y2 = y + h
 
 method center(self: Rect) : tuple[x : int, y : int] =
+  # returns the center of this rectangle
   var center_x = (self.x1 + self.x2) div 2
   var center_y = (self.y1 + self.y2) div 2
   result = (center_x, center_y)
@@ -75,14 +92,50 @@ proc create_room(room : Rect) =
       map[i][j].block_sight = false
 
 proc create_h_tunnel(x1 : int, x2 : int, y : int) =
+  #horizontal tunnel. min() and max() are used in case x1>x2
   for x in min(x1, x2)..(max(x1, x2) + 1):
     map[x][y].blocked = false
     map[x][y].block_sight = false
 
 proc create_v_tunnel(y1 : int, y2 : int, x : int) =
+  #vertical tunnel
   for y in min(y1, y2)..(max(y1, y2) + 1):
     map[x][y].blocked = false
     map[x][y].block_sight = false
+
+#########################################################################
+# Character
+#########################################################################
+
+method move(self : Character, dx : int, dy : int) =
+  #move by the given amount, if the destination is not blocked
+  if not map[self.x + dx][self.y + dy].blocked:
+    self.x += dx
+    self.y += dy
+
+method draw(self : Character) =
+  # draw the character that represents this object at its position
+  console_set_default_foreground(main_console, self.color)
+  console_put_char(main_console, self.x, self.y, self.symbol, BKGND_NONE)
+
+method clear(self : Character) =
+  console_put_char(main_console, self.x, self.y, ' ', BKGND_NONE)
+  
+#########################################################################
+# Internal procs
+#########################################################################
+
+proc render_all() =
+  # go through all tiles and set their background color
+  for i in 0..<MAP_WIDTH:
+    for j in 0..<MAP_HEIGHT:
+      if map[i][j].block_sight:
+        console_set_char_background(main_console, i, j, COLOR_DARK_WALL, BKGND_SET)
+      else:
+        console_set_char_background(main_console, i, j, COLOR_DARK_GROUND, BKGND_SET)
+        
+  player.draw()
+  console_blit(main_console, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, nil, 0, 0, 1.0, 1.0)
 
 proc make_map =
   # fill map with "blocked" tiles
@@ -137,33 +190,7 @@ proc make_map =
       rooms.add(new_room)
       num_rooms += 1
 
-method move(self : Character, dx : int, dy : int) =
-  if not map[self.x + dx][self.y + dy].blocked:
-    self.x += dx
-    self.y += dy
-
-method draw(self : Character) =
-  console_set_default_foreground(main_console, self.color)
-  console_put_char(main_console, self.x, self.y, self.symbol, BKGND_NONE)
-
-method clear(self : Character) =
-  console_put_char(main_console, self.x, self.y, ' ', BKGND_NONE)
-  
-
-proc init*(title : string, message: string) : void =
-  console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, title, false)
-  main_console = console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
-  sys_set_fps(LIMIT_FPS)
-
-  player =  Character(x : 0, y : 0, color : RED, symbol : '@')
-
-  make_map()
-
-  console_clear(main_console)
-  #discard console_print_rect_ex(main_console, SCREEN_WIDTH_2, 3, SCREEN_WIDTH, 0, BKGND_NONE, CENTER, message)
-  
-  
-proc handle_input*() : bool =
+proc handle_input() : bool =
   discard sys_wait_for_event(EVENT_KEY_PRESS or EVENT_MOUSE, addr(key), addr(mouse), true)
   result = true
   case key.vk
@@ -180,19 +207,22 @@ proc handle_input*() : bool =
   else:
     result = true
 
-proc render_all() =
-  # go through all tiles and set their background color
-  for i in 0..<MAP_WIDTH:
-    for j in 0..<MAP_HEIGHT:
-      if map[i][j].block_sight:
-        console_set_char_background(main_console, i, j, color_dark_wall, BKGND_SET)
-      else:
-        console_set_char_background(main_console, i, j, color_dark_ground, BKGND_SET)
-        
-  player.draw()
-  console_blit(main_console, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, nil, 0, 0, 1.0, 1.0)
+#########################################################################
+# Exported procs
+#########################################################################
 
+proc init*(title : string, message: string) : void =
+  console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, title, false)
+  main_console = console_new(SCREEN_WIDTH, SCREEN_HEIGHT)
+  sys_set_fps(LIMIT_FPS)
 
+  player =  Character(x : 0, y : 0, color : RED, symbol : '@')
+
+  make_map()
+
+  console_clear(main_console)
+  #discard console_print_rect_ex(main_console, SCREEN_WIDTH_2, 3, SCREEN_WIDTH, 0, BKGND_NONE, CENTER, message)
+  
 proc main_loop*() : void =
   while not console_is_window_closed():
 
